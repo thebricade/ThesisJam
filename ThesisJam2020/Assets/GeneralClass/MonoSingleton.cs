f207769be2152fdcd2d13c281933664219af836a
Yukii <yukiizzzx@gmail.com>
2019-09-01 13:39:47 -0400

    Plugins & dirty files

---
+++ ThesisJam2020/Assets/GeneralClass/MonoSingleton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = Object.FindObjectOfType<T>();
+            }
+            return instance;
+        }
+    }
+}
