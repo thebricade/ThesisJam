4b8e75515237b826e53f6923520f1146a12e713f
Bri <bs3864@nyu.edu>
2019-09-01 13:40:35 -0400

    More assets

--- ThesisJam2020/Assets/Scripts/PointInteraction.cs
+++ ThesisJam2020/Assets/Scripts/PointInteraction.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PointInteraction : MonoBehaviour
 {
 
-    //This script will be for objects that you can interactive with and trigger different cameras effects (zoom) or cuts depending on the object
+    //This script will be for objects that you can interactive with and trigger different cameras effects (zoom) or cuts depending on the object 
     
     // Start is called before the first frame update
     void Start()
