617b222018992c5e42235795b34e392a86677e7f
Bri <bs3864@nyu.edu>
2019-09-01 15:02:08 -0400

    Fixed fade with window and implemented all the color fade in

--- ThesisJam2020/Assets/Scripts/PointInteraction.cs
+++ ThesisJam2020/Assets/Scripts/PointInteraction.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class PointInteraction : MonoBehaviour
 {
+    private SpriteEffect _spriteEffect, _spriteEffectPair;
 
-    //This script will be for objects that you can interactive with and trigger different cameras effects (zoom) or cuts depending on the object 
+    public GameObject pairObject; 
+   // public GameObject mainWindowOutline;
+    
+    //This script will be for objects that you can interactive wit3-----------
+    //h and trigger different cameras effects (zoom) or cuts depending on the object 
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _spriteEffect = gameObject.AddComponent<SpriteEffect>();
+        _spriteEffectPair = pairObject.AddComponent<SpriteEffect>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnMouseOver()
+    {
+        _spriteEffect.numberAssignment(1.2f,SpriteEffect.EffectType.FadeImage,0);
+        _spriteEffectPair.numberAssignment(1.2f,SpriteEffect.EffectType.ShowImage,0);
+        //if(this.GetComponent<SpriteRenderer>().color)
     }
 }
