a798e9d45def7bd59ed88111bf60aa7e5c78dcec
Yukii <yukiizzzx@gmail.com>
2019-09-05 18:29:57 -0400

    Light-prototype

---
+++ ThesisJam2020/Assets/Scripts/CameraFunc.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFunc : MonoSingleton<CameraFunc>
+{
+    public GameObject[] camera1 = new GameObject[3];
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void ChangeCam(int i)
+    {
+        for (int x = 0; x < camera1.Length; x++)
+        {
+            if (camera1[i] != null)
+            {
+                if(x !=i ) camera1[x].SetActive(false);
+                else camera1[x].SetActive(true);
+            }
+        }
+    }
+}
