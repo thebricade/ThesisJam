a1db962fdbe774b4971177db87cdaa9a412a7670
Brianna <bs3864@nyu.edu>
2019-09-12 10:36:46 -0400

    New Videos
    Created a new scene for the car.

---
+++ ThesisJam2020/Assets/Scripts/PlayReverse.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayReverse : MonoBehaviour
+{
+    public GameObject VideoTrial, VideoTrialBackwards;
+    public GameObject CameraForward, CameraBackwards; 
+    private VideoPlayer trainVideo, trainVideoBackwards;
+
+    private bool playingForward; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        trainVideo = VideoTrial.GetComponent<VideoPlayer>();
+        trainVideoBackwards = VideoTrialBackwards.GetComponent<VideoPlayer>();
+        trainVideo.time = 1;
+        trainVideoBackwards.time = 1; 
+        playingForward = true;
+        print(trainVideoBackwards.length);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void OnMouseEnter()
+    {
+        if (playingForward)
+        {
+            CameraForward.SetActive(true);
+            CameraBackwards.SetActive(false);
+            trainVideo.playbackSpeed = 1;
+        }
+        else
+        {
+            CameraForward.SetActive(false);
+            CameraBackwards.SetActive(true);
+             
+            trainVideoBackwards.playbackSpeed = 1; 
+
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (playingForward)
+        {
+            trainVideo.playbackSpeed = 0;
+            trainVideoBackwards.playbackSpeed = 0; 
+            trainVideoBackwards.time = trainVideoBackwards.length -trainVideo.time;
+            playingForward = false;
+        }
+        else
+        {
+            trainVideoBackwards.playbackSpeed = 0; 
+            trainVideo.playbackSpeed = 0;
+            //trainVideo.time = trainVideo.length - trainVideoBackwards.time;
+            //playingForward = true;
+        }
+               
+        //VideoTrial.SetActive(false);
+        //VideoTrialBackwards.SetActive(true);
+        //trainVideoBackwards.timeSource = trainVideo.time-trainVideoBackwards.length; 
+
+
+    }
+}
--- ThesisJam2020/Assets/Scripts/PointInteraction.cs
+++ ThesisJam2020/Assets/Scripts/PointInteraction.cs
@@ -6,7 +6,9 @@ public class PointInteraction : MonoBehaviour
 {
     private SpriteEffect _spriteEffect, _spriteEffectPair;
 
-    public GameObject pairObject; 
+    public GameObject pairObject;
+   
+    
    // public GameObject mainWindowOutline;
     
     //This script will be for objects that you can interactive wit3-----------
@@ -17,6 +19,8 @@ public class PointInteraction : MonoBehaviour
     {
         _spriteEffect = gameObject.AddComponent<SpriteEffect>();
         _spriteEffectPair = pairObject.AddComponent<SpriteEffect>();
+        
+        
 
     }
 
@@ -28,8 +32,14 @@ public class PointInteraction : MonoBehaviour
 
     private void OnMouseOver()
     {
-        _spriteEffect.numberAssignment(1.2f,SpriteEffect.EffectType.FadeImage,0);
-        _spriteEffectPair.numberAssignment(1.2f,SpriteEffect.EffectType.ShowImage,0);
+        _spriteEffect.numberAssignment(0.2f,SpriteEffect.EffectType.FadeImage,0);
+        _spriteEffectPair.numberAssignment(0.2f,SpriteEffect.EffectType.ShowImage,0);
         //if(this.GetComponent<SpriteRenderer>().color)
+    }
+
+    public void attack()
+    {
+        
+        // pairObject.GetComponent<Transform>().position = 2; 
     }
 }
